8dd645aa15faa545d0eda96bf10aeee1af624f27
zen0-5338 <shahjeet767@gmail.com>
2024-05-15 22:30:22 +0530

    fix: import paths for sub-modules

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,5 +1,5 @@
-from game_button import GameButton
-from load_sprite import load_sprite
-from scale_coordinates import scale_coordinates, SCREEN_HEIGHT, SCREEN_WIDTH
-from sprite_paths import GAME_BACKGROUND_PATH
-from text_box import TextBox
+from .game_button import GameButton
+from .load_sprite import load_sprite
+from .scale_coordinates import scale_coordinates, SCREEN_HEIGHT, SCREEN_WIDTH
+from .sprite_paths import GAME_BACKGROUND_PATH
+from .text_box import TextBox
