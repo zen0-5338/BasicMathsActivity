45f8b7a0168df0b47d4fc0ede312f752bc797ad4
zen0-5338 <shahjeet767@gmail.com>
2024-05-15 22:36:45 +0530

    fix: import order in ui module

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,5 +1,6 @@
-from .game_button import GameButton
-from .load_sprite import load_sprite
+from .fonts import GameFont
 from .scale_coordinates import scale_coordinates, SCREEN_HEIGHT, SCREEN_WIDTH
 from .sprite_paths import GAME_BACKGROUND_PATH
+from .game_button import GameButton
+from .load_sprite import load_sprite
 from .text_box import TextBox
