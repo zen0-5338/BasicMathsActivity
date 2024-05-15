6b6caf6542dd5d6c819923ba1eb0dd762c062295
zen0-5338 <shahjeet767@gmail.com>
2024-05-15 22:38:03 +0530

    fix: final fix for sub-module import order in UI

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,5 +1,5 @@
-from .fonts import GameFont
 from .scale_coordinates import scale_coordinates, SCREEN_HEIGHT, SCREEN_WIDTH
+from .fonts import GameFont
 from .sprite_paths import GAME_BACKGROUND_PATH
 from .game_button import GameButton
 from .load_sprite import load_sprite
